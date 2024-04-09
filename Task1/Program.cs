cb08b229ae8b36e6dcb298ae879f6d0c2d7540c4
Fry <danil.demyanovich@gmail.com>
2024-04-09 18:43:54 +0300

    Initial C# Attestation

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine()!);
+
+LineOutput(m, n);
+
+void LineOutput(int number1, int number2)
+{
+    if (number2 == number1)
+    {
+        return;
+    }
+    else
+    {
+        LineOutput(number1, number2 - 1);
+        Console.Write($"{number2} ");
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+int[] array = new int[] { 1, 2, 5, 0, 10, 34 };
+
+void ReversArrayOutput(int[] array, int i = 5)
+{
+    if (i < 0)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(" " + array[i--]);
+        ReversArrayOutput(array, i);
+    }
+}
+
+ReversArrayOutput(array);
